907a7325489c0192295a8e512d4232e738845b36
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2025-04-06 22:56:15 -0700

    Commit message: 完成云端部署相关代码

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from 'express'
+import mongoose from "mongoose";
+
 import Lab5 from "./Lab5/index.js";
 import Hello from "./Hello.js"
 import cors from "cors";
@@ -8,6 +10,9 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRotes from "./Kambaz/Enrollments/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 
@@ -25,7 +30,7 @@ if (process.env.NODE_ENV !== "development") {
     sessionOptions.cookie = {
       sameSite: "none",
       secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
+      // domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
 app.use(session(sessionOptions));
